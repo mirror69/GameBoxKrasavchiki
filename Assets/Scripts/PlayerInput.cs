a21db5f28d9bb07a3f95ee5b5e47f35c3df2b70e
Andrey <1929999@gmail.com>
2021-07-01 20:47:07 +0300

    Добавил скрипт движения игрока + инпут

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerInput : MonoBehaviour
+{
+    private Vector3 moveDirection;
+    private Vector3 rotateDirection;
+    private float horizontalInput;
+    private float verticalInput;
+    private PlayerController playerController;    
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        rotateDirection = Input.mousePosition;
+    }
+
+    private void FixedUpdate()
+    {
+        moveDirection = new Vector3(-horizontalInput, 0, -verticalInput);        
+
+        playerController.MovePlayer(moveDirection);
+        playerController.RotatePlayer(rotateDirection);
+    }
+}
