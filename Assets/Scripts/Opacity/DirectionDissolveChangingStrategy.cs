f5833457698a7ac0d7e6e5d55979c145daf6fb10
Alexander Shishkin <sahanich1@gmail.com>
2021-07-08 17:32:16 +0300

    D6. Добавлена индикация цветом при полном исчезновении объекта

--- Assets/Scripts/Opacity/DirectionDissolveChangingStrategy.cs
+++ Assets/Scripts/Opacity/DirectionDissolveChangingStrategy.cs
@@ -5,12 +5,27 @@
 /// </summary>
 public class DirectionDissolveChangingStrategy: OpacityChangingStrategy
 {
+    private const float HighestDissolveOffset = float.MaxValue;
+
     [Tooltip("Минимальное значение смещения эффекта растворения")]
     [SerializeField]
     private float minDirectionDissolveOffset;
+    
     [Tooltip("Максимальное значение смещения эффекта растворения")]
     [SerializeField]
     private float maxDirectionDissolveOffset;
+
+    [Tooltip("Цвет границы эффекта растворения по умолчанию")]
+    [SerializeField]
+    [ColorUsageAttribute(true, true)]
+    private Color defaultDissolveEdgeColor;
+
+    [Tooltip("Цвет границы эффекта растворения для обозначения полностью невидимого объекта")]
+    [SerializeField]
+    [ColorUsageAttribute(true, true)]
+    private Color fullDissolvedEdgeColor;
+
+    private float currentDissolveOffset;
 
     public float MinDirectionDissolveOffset => minDirectionDissolveOffset;
     public float MaxDirectionDissolveOffset => maxDirectionDissolveOffset;
@@ -18,6 +33,13 @@ public class DirectionDissolveChangingStrategy: OpacityChangingStrategy
     public override void SetOpacity(Material material, float value)
     {
         material.SetOpacity(value);
-        material.SetDirectionDissolve(value, MinDirectionDissolveOffset, MaxDirectionDissolveOffset);
+
+        currentDissolveOffset = value >= 1 ? HighestDissolveOffset :
+            minDirectionDissolveOffset + (maxDirectionDissolveOffset - minDirectionDissolveOffset) * value;
+
+        Color edgeColor = currentDissolveOffset <= minDirectionDissolveOffset ?
+            fullDissolvedEdgeColor : defaultDissolveEdgeColor;
+
+        material.SetDirectionDissolve(currentDissolveOffset, edgeColor);
     }
 }
